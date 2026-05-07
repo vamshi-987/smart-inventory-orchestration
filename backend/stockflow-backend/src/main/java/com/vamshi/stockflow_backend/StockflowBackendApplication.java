3597330ffa01730f7f1f26b5df69781898306e39
vamshi-987 <vamshivardhan987@gmail.com>
2026-05-07 11:11:55 +0530

    Add Spring Boot backend setup

---
+++ backend/stockflow-backend/src/main/java/com/vamshi/stockflow_backend/StockflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vamshi.stockflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockflowBackendApplication.class, args);
+	}
+
+}
---
+++ backend/stockflow-backend/src/test/java/com/vamshi/stockflow_backend/StockflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamshi.stockflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
